ecbf33790cb566f56de75666a45053b10056a034
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2016-04-14 09:43:02 -0300

    Add konami code with ryu giving his hadouken

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+/* globals cheet: true, $: false */
+
+'use strict';
+
+console.log('==============CM42==============');
+console.log('konami code: ↑ ↑ ↓ ↓ ← → ← → b a');
+console.log('================================');
+
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+  $('body').append('<div class="cheet"></div>');
+  setTimeout(function () {
+    $('.cheet').toggleClass('cheet--without-gif');
+    setTimeout(function () {
+      $('.cheet').remove();
+    }, 1000);
+  }, 4000);
+});
