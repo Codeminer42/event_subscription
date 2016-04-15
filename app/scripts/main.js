78f8332ae3fb7f913b3c4828c88eaecec26cfb87
Pedro Felipe <pedro@pedrofelipe.com.br>
2016-04-14 23:09:30 -0300

    Polindo as arestas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,22 +2,29 @@
 
 'use strict';
 
-console.log('==============CM42==============');
-console.log('konami code: ↑ ↑ ↓ ↓ ← → ← → b a');
-console.log('================================');
+$(document).ready(function() {
+  console.log('@bernardo left an easter egg here 😶');
+  console.log('↑ ↑ ↓ ↓ ← → ← → b a');
 
-cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-  $('body').append('<div class="cheet"></div>');
-  setTimeout(function () {
-    $('.cheet').toggleClass('cheet--without-gif');
-    setTimeout(function () {
-      $('.cheet').remove();
-    }, 1000);
-  }, 4000);
-});
+  cheet('↑ ↑ ↓ ↓ ← → ← → b a', function() {
+    $('body').append('<div class="cheet"></div>');
+    setTimeout(function() {
+      $('.cheet').toggleClass('cheet--without-gif');
+      setTimeout(function() {
+        $('.cheet').remove();
+      }, 1000);
+    }, 4000);
+  });
 
-$(document).ready(function(){
-  $('form').submit(function(e) {
-    $(".result").text("deu certo ;)");
+  $('form').submit(function() {
+    $('.subscribe__success__name').html($('#subscribeName').val());
+    $('.subscribe__form, .subscribe__success').toggle();
+  });
+
+  $('.subscribe__success a').click(function(e) {
+    e.preventDefault();
+
+    $('#subscribeName, #subscribeEmail').val('');
+    $('.subscribe__form, .subscribe__success').toggle();
   });
 });
